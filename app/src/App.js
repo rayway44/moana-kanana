221a75fc1f34db6b1e7fafba34db34f7a0363b9c
Stuart Toleafoa <stuarttoley@gmail.com>
2022-04-07 22:25:11 +1200

    tinker

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,11 @@
 
 import './App.css';
-<<<<<<< HEAD
 import Home from './Pages/Home'
-=======
-import MainSectionP2 from './Components/MainSection/MainSectionP2';
-import TNavBar from './Components/TNavbar/TNavBar';
->>>>>>> stuFeature
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Home />
-=======
-      {/* <TNavBar /> */}
-      <MainSectionP2 />
->>>>>>> stuFeature
     </div>
   )
 }
