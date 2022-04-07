ac192836866dd7ecb02b3fd130aa5b7b8a1e6aff
Stuart Toleafoa <stuarttoley@gmail.com>
2022-04-07 16:06:41 +1200

    first push

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MainSectionP2 from './Components/MainSection/MainSectionP2';
+import TNavBar from './Components/TNavbar/TNavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <TNavBar /> */}
+      <MainSectionP2 />
     </div>
-  );
+  )
 }
 
 export default App;
