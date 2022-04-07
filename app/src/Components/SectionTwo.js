9347662410fe8c1beb0b592fc768200152b02da3
Ray Letoa <rayletoa@RAYway-2.local>
2022-04-08 09:52:06 +1200

    icons from section two - three icons locked in

--- app/src/Components/SectionTwo.js
+++ app/src/Components/SectionTwo.js
@@ -3,8 +3,8 @@ import './css/SectionTwo.css'
 
 export default function SectionTwo() {
   return (
-    <div className='section-two-holder'>
-
+    <div className="section-two-main">
+      <div className='section-two-holder'>
         <div className='section-two-top'>
             
               <div className='section-two-one'>
@@ -21,7 +21,16 @@ export default function SectionTwo() {
         <div className='section-two-content'>
         Kanana Ltd. is a mentoring company offering adult training and project management services to the Paciific (Pasifika) communities in order to help them achieve a better life quality. Kanana’s two milestones are Project Ikuna, offering training for Pacific employers; and Project No Place Like Home, aimed at home ownership.
         </div>
+      </div>
 
+        <div className='section-two-bottom-img-holder'>
+          <div className='section-two-bottom-img-one'>
+          <img src="Images/BgPacificPattern03.png" alt="" />
+          </div>
+          <div className='section-two-bottom-img-two'>
+          <img src="Images/BgPacificPattern04.png" alt="" />
+          </div>
+        </div>
     </div>
   )
 }
