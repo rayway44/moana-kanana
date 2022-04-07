38aa4ed5efbf6a36a528cafc508bf72ef00a44f4
Stuart Toleafoa <stuarttoley@gmail.com>
2022-04-07 22:20:45 +1200

    changes

--- app/src/App.js
+++ app/src/App.js
@@ -5,24 +5,7 @@ import Home from './Pages/Home'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Updated text
-        </a>
-      </header>
-=======
       <Home />
->>>>>>> Neish
     </div>
   );
 }
