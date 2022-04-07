33ebb1068f09076c3d36716257a933685bc623bb
wolfgangcrusader <nlopati@gmail.com>
2022-04-07 16:43:36 +1200

    second section done

--- app/src/Pages/Home.js
+++ app/src/Pages/Home.js
@@ -5,6 +5,8 @@ import Nav from '../Components/NavBar'
 import SectionOne from '../Components/SectionOne'
 import SectionTwo from '../Components/SectionTwo'
 import SectionThree from '../Components/SectionThree';
+import SectionFour from '../Components/SectionFour';
+import Testimonials from '../Components/Testimonials'
 
 export default function Home() {
 
@@ -14,7 +16,9 @@ export default function Home() {
       <Nav />
       <SectionOne />
       <SectionTwo />
-      <SectionThree />       
+      <SectionThree />
+      <SectionFour/> 
+      <Testimonials />     
     </div>
   )
 }
