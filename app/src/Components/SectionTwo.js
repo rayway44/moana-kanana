7e46c946259ec331a27588e9593b407a4409831b
Ray Letoa <rayletoa@RAYway-2.local>
2022-04-07 13:18:53 +1200

    section one and two done

--- app/src/Components/SectionTwo.js
+++ app/src/Components/SectionTwo.js
@@ -4,9 +4,11 @@ import './css/SectionTwo.css'
 export default function SectionTwo() {
   return (
     <div className='section-two-holder'>
+
+    
         
         <div className='section-two-title'>
-        who we are
+            WHO WE ARE
         </div>
         <div className='section-two-content'>
         Kanana Ltd. is a mentoring company offering adult training and project management services to the Paciific (Pasifika) communities in order to help them achieve a better life quality. Kanana’s two milestones are Project Ikuna, offering training for Pacific employers; and Project No Place Like Home, aimed at home ownership.
--- app/src/Pages/Home.js
+++ app/src/Pages/Home.js
@@ -4,6 +4,7 @@ import Header from '../Components/Header'
 import Nav from '../Components/NavBar'
 import SectionOne from '../Components/SectionOne'
 import SectionTwo from '../Components/SectionTwo'
+import SectionThree from '../Components/SectionThree'
 
 export default function Home() {
 
