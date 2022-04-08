3eb21f991681d9d5720e657aa7d8e5d9face0d5e
Stuart Toleafoa <stuarttoley@gmail.com>
2022-04-08 15:55:32 +1200

    tweaks

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './Pages/Home'
+import About from './Pages/About';
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
-import About from './Pages/About';
+
 
 function App() {
   return (
@@ -15,9 +16,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
-            
         </Routes>
-
       </Router>
     </div>
   )
