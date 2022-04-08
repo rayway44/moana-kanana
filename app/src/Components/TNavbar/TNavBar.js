da712f69aeaba60d2acb0d22f9d817498f5f7657
Ray Letoa <rayletoa@RAYway-2.local>
2022-04-08 20:05:05 +1200

    nav tab done

--- app/src/Components/TNavbar/TNavBar.js
+++ app/src/Components/TNavbar/TNavBar.js
@@ -10,18 +10,18 @@ export default function TNavBar() {
       <div className="tnav">
 
         <div className="tnav-left">
-          <img src={Logo} alt='' />
+        <img src={Logo} alt='' />
           <div>
-            <span className="tleft-title">KANANA</span><br />
+            <span className="tleft-title"><a href=' '>KANANA</a></span><br />
             Building Better Futures
           </div>
         </div>
 
         <div className="tnav-right">
           <div className="tnav-icon"><Link to='/'>Home</Link></div>
-          <div className="tnav-icon">About</div>
-          <div className="tnav-icon">Services</div>
-          <div>Contact</div>
+          <div className="tnav-icon"><a href=' '>About</a></div>
+          <div className="tnav-icon"><a href=' '>Services</a></div>
+          <div><a href=' '>Contact</a></div>
         </div>
       
       </div>
